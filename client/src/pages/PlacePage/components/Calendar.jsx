cfb3ab523b788f0e0578d302395bee8b4eba49a9
ARUN KUMAR D <arun14k08@gmail.com>
2024-05-10 13:20:32 +0530

    fix: encapsulated dates into UI components, working to make date selection effectively

--- client/src/pages/PlacePage/components/Calendar.jsx
+++ client/src/pages/PlacePage/components/Calendar.jsx
@@ -1,7 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { days, months } from "../lib/calendar";
 import { LeftIcon, RightIcon } from "../assets/SVGAssets";
-import { format } from "date-fns";
+import { format, parseISO } from "date-fns";
+import {
+    BlankDate,
+    CheckInDate,
+    CheckOutDate,
+    ClearDates,
+    DisabledDate,
+    SelectableDate,
+    SelectedDate,
+} from "../lib/UIComponents";
 
 const Calendar = ({
     checkInDate,
@@ -22,7 +31,14 @@ const Calendar = ({
     const [numberOfDays, setNumberOfDays] = useState(
         new Date(year, month, 0).getDate()
     );
+    const [minimumDateToCheckOut, setMinimumDateToCheckOut] = useState();
     const [maximumDateToCheckOut, setMaximumDateToCheckOut] = useState();
+
+    useEffect(() => {
+        let days = (parseISO(checkOutDate) - parseISO(checkInDate)) / 86400000;
+        setTotalDays(days);
+        setTotalPrice(days * price);
+    }, [checkInDate, checkOutDate]);
 
     const handleRightClick = () => {
         const nextMonth = month === 12 ? 1 : month + 1;
@@ -53,71 +69,25 @@ const Calendar = ({
     };
 
     const handleDateSelect = ({ day, month, year }) => {
-        const formattedDate = format(
-            new Date(year, month - 1, day),
-            "yyyy-MM-dd"
-        );
-
-        if (!checkInDate) {
-            setCheckInDate(formattedDate);
-            // set maximum date that can be selected
-            if (!maximumDateToCheckOut) {
-                let maxDate = new Date(
-                    year,
-                    month - 1,
-                    day + maximumBookingDays
-                );
-                setMaximumDateToCheckOut(format(maxDate, "yyyy-MM-dd"));
-            }
-
-            // select minimum date to check out by default
-            const minimumDateToCheckOut = format(
+        const date = new Date(year, month - 1, day);
+        const formattedDate = format(date, "yyyy-MM-dd");
+        let minimumCheckOut = "";
+        let maximumCheckOut = "";
+        let SelectedBeforeOldCheckIn = date < parseISO(checkInDate);
+        //  selecting a date before the check-in date (or) check-in is not selected
+        if (SelectedBeforeOldCheckIn || !checkInDate) {
+            minimumCheckOut = format(
                 new Date(year, month - 1, day + minimumBookingDays),
                 "yyyy-MM-dd"
             );
-            setCheckOutDate(minimumDateToCheckOut);
-            const days =
-                (new Date(minimumDateToCheckOut) - new Date(formattedDate)) /
-                86400000;
-            setTotalDays(days);
-            setTotalPrice(days * price);
-            return;
-        }
-
-        if (new Date(formattedDate) < new Date(checkInDate)) {
-            let maxDate = new Date(
-                year,
-                month - 1,
-                new Date(formattedDate).getDate() + maximumBookingDays
+            maximumCheckOut = format(
+                new Date(year, month - 1, day + maximumBookingDays),
+                "yyyy-MM-dd"
             );
-            setMaximumDateToCheckOut(format(maxDate, "yyyy-MM-dd"));
             setCheckInDate(formattedDate);
-            if (maxDate < new Date(checkOutDate)) {
-                setCheckOutDate(format(maxDate, "yyyy-MM-dd"));
-            }
-            let minimumDateToCheckOut = "";
-            if (!checkOutDate) {
-                minimumDateToCheckOut = format(
-                    new Date(year, month - 1, day + minimumBookingDays),
-                    "yyyy-MM-dd"
-                );
-                setCheckOutDate(minimumDateToCheckOut);
-            }
-            const days =
-                (new Date(checkOutDate || minimumDateToCheckOut || maxDate) -
-                    new Date(formattedDate)) /
-                86400000;
-            setTotalDays(days);
-            setTotalPrice(days * price);
-            return;
-        }
-
-        if (formattedDate > checkOutDate) {
-            setCheckOutDate(formattedDate);
-            const days =
-                (new Date(formattedDate) - new Date(checkInDate)) / 86400000;
-            setTotalDays(days);
-            setTotalPrice(days * price);
+            setMinimumDateToCheckOut(minimumCheckOut);
+            setMaximumDateToCheckOut(maximumCheckOut);
+            setCheckOutDate(minimumCheckOut);
             return;
         }
     };
@@ -161,46 +131,25 @@ const Calendar = ({
                     );
                 })}
                 {[...Array(blankDays)].map((value, index) => {
-                    return (
-                        <div key={index} className="px-2 py-1">
-                            &nbsp;
-                        </div>
-                    );
+                    // blank days
+                    return <BlankDate key={index} />;
                 })}
                 {[...Array(numberOfDays)].map((value, index) => {
                     // past dates disabled by default
                     if (new Date(year, month - 1, index + 1) < new Date()) {
-                        return (
-                            <p
-                                key={index}
-                                className="py-3 flex transition-all  justify-center items-center cursor-pointer rounded-full ring-black"
-                            >
-                                <s className="text-slate-300">{index + 1}</s>
-                            </p>
-                        );
+                        return <DisabledDate key={index} index={index} />;
+                    }
+
+                    // dates beyond maximum selectable date
+                    if (
+                        maximumDateToCheckOut &&
+                        parseISO(maximumDateToCheckOut) <
+                            new Date(year, month - 1, index + 1)
+                    ) {
+                        return <DisabledDate key={index} index={index} />;
                     }
 
                     // highlight the range of dates
-
-                    // check in date without checkOutDate selected
-                    if (
-                        format(
-                            new Date(year, month - 1, index + 1),
-                            "yyyy-MM-dd"
-                        ) === checkInDate &&
-                        checkOutDate
-                    ) {
-                        return (
-                            <p
-                                key={index}
-                                className="py-3 flex transition-all  justify-center items-center cursor-pointer rounded-full rounded-r-none bg-black text-white bg-gradient-to-r from-black to-slate-400"
-                            >
-                                {index + 1}
-                            </p>
-                        );
-                    }
-
-                    // check in date with checkOutDate selected
 
                     // check in date
                     if (
@@ -209,14 +158,7 @@ const Calendar = ({
                             "yyyy-MM-dd"
                         ) === checkInDate
                     ) {
-                        return (
-                            <p
-                                key={index}
-                                className="py-3 flex transition-all  justify-center items-center cursor-pointer rounded-full bg-black text-white"
-                            >
-                                {index + 1}
-                            </p>
-                        );
+                        return <CheckInDate key={index} index={index} />;
                     }
 
                     // check out date
@@ -226,80 +168,37 @@ const Calendar = ({
                             "yyyy-MM-dd"
                         ) === checkOutDate
                     ) {
-                        return (
-                            <p
-                                key={index}
-                                className="py-3 flex transition-all  justify-center items-center cursor-pointer rounded-full bg-gradient-to-l from-black to-slate-400 text-white rounded-l-none"
-                            >
-                                {index + 1}
-                            </p>
-                        );
+                        return <CheckOutDate key={index} index={index} />;
                     }
 
                     // between dates
 
                     if (
-                        maximumDateToCheckOut &&
-                        new Date(maximumDateToCheckOut) <
-                            new Date(year, month - 1, index + 1)
-                    ) {
-                        return (
-                            <p
-                                key={index}
-                                className="py-3 flex transition-all  justify-center items-center cursor-pointer rounded-full ring-black"
-                            >
-                                <s className="text-slate-300">{index + 1}</s>
-                            </p>
-                        );
-                    }
-                    if (
-                        new Date(checkInDate) <
+                        parseISO(checkInDate) <
                             new Date(year, month - 1, index + 2) &&
                         new Date(year, month - 1, index + 1) <
-                            new Date(checkOutDate)
+                            parseISO(checkOutDate)
                     ) {
-                        return (
-                            <p
-                                key={index}
-                                className="py-3 flex transition-all  justify-center items-center cursor-pointer -mx-2 bg-slate-400 text-white"
-                            >
-                                {index + 1}
-                            </p>
-                        );
+                        return <SelectedDate key={index} index={index} />;
                     }
 
                     // selectable dates
                     return (
-                        <>
-                            <button
-                                key={index}
-                                className="py-3 flex transition-all  justify-center items-center cursor-pointer rounded-full hover:bg-slate-200  hover:ring-1 hover:ring-black"
-                                onClick={(event) => {
-                                    event.preventDefault();
-                                    handleDateSelect({
-                                        day: index + 1,
-                                        month,
-                                        year,
-                                    });
-                                }}
-                            >
-                                {index + 1}
-                            </button>
-                        </>
+                        <SelectableDate
+                            key={index}
+                            index={index}
+                            handleDateSelect={handleDateSelect}
+                            year={year}
+                            month={month}
+                        />
                     );
                 })}
             </div>
-            <button
-                onClick={(event) => {
-                    setCheckInDate("");
-                    setCheckOutDate("");
-                    setMaximumDateToCheckOut("");
-                    event.preventDefault();
-                }}
-                className="underline font-semibold mt-2 hover:bg-slate-200 rounded-lg px-2 py-1"
-            >
-                Clear Dates
-            </button>
+            <ClearDates
+                setCheckInDate={setCheckInDate}
+                setCheckOutDate={setCheckOutDate}
+                setMaximumDateToCheckOut={setMaximumDateToCheckOut}
+            />
         </div>
     );
 };
---
+++ client/src/pages/PlacePage/lib/UIComponents.jsx
@@ -0,0 +1,76 @@
+export const BlankDate = () => {
+    return <div className="px-2 py-1">&nbsp;</div>;
+};
+
+export const DisabledDate = ({ index }) => {
+    return (
+        <p className="py-3 flex transition-all  justify-center items-center cursor-pointer rounded-full ring-black">
+            <s className="text-slate-300">{index + 1}</s>
+        </p>
+    );
+};
+
+export const SelectableDate = ({ index, month, year, handleDateSelect }) => {
+    return (
+        <button
+            className="py-3 flex transition-all  justify-center items-center cursor-pointer rounded-full hover:bg-slate-200  hover:ring-1 hover:ring-black"
+            onClick={(event) => {
+                event.preventDefault();
+                handleDateSelect({
+                    day: index + 1,
+                    month,
+                    year,
+                });
+            }}
+        >
+            {index + 1}
+        </button>
+    );
+};
+
+export const SelectedDate = ({ index }) => {
+    return (
+        <p className="py-3 flex transition-all  justify-center items-center cursor-pointer -mx-2 bg-slate-400 text-white">
+            {index + 1}
+        </p>
+    );
+};
+
+export const CheckInDate = ({ index }) => {
+    return (
+        <p className="py-3 flex transition-all  justify-center items-center cursor-pointer rounded-full bg-gradient-to-r from-black to-slate-400 text-white rounded-r-none">
+            {index + 1}
+        </p>
+    );
+};
+
+export const CheckOutDate = ({ index }) => {
+    return (
+        <p
+            key={index}
+            className="py-3 flex transition-all  justify-center items-center cursor-pointer rounded-full bg-gradient-to-l from-black to-slate-400 text-white rounded-l-none"
+        >
+            {index + 1}
+        </p>
+    );
+};
+
+export const ClearDates = ({
+    setCheckInDate,
+    setCheckOutDate,
+    setMaximumDateToCheckOut,
+}) => {
+    return (
+        <button
+            onClick={(event) => {
+                setCheckInDate("");
+                setCheckOutDate("");
+                setMaximumDateToCheckOut("");
+                event.preventDefault();
+            }}
+            className="underline font-semibold mt-2 hover:bg-slate-200 rounded-lg px-2 py-1"
+        >
+            Clear Dates
+        </button>
+    );
+};
