fa86929699695eeff5052086429b41ae89ddbfb3
ARUN KUMAR D <arun14k08@gmail.com>
2024-05-14 14:21:46 +0530

    fix: added more info in the specific place page

--- client/src/pages/PlacePage/PlacePage.jsx
+++ client/src/pages/PlacePage/PlacePage.jsx
@@ -7,6 +7,10 @@ import Calendar from "./components/Calendar";
 import { UserContext } from "../../context/UserContextProvider";
 import { format } from "date-fns";
 import BookingForm from "./components/BookingForm";
+import { ProfileIcon } from "../../components/NavBar/assets/SVGAssets";
+import { ProfileIconBig } from "./assets/SVGAssets";
+import { RoomIcon } from "../DashBoard/assets/SVGAssets";
+import { BedIcon } from "../DashBoard/assets/ImageAssets";
 const PlacePage = () => {
     const { id } = useParams();
     const [place, setPlace] = useState();
@@ -51,7 +55,7 @@ const PlacePage = () => {
     };
 
     return (
-        <div className="mx-24">
+        <div className="mx-24 mb-12">
             <div className="flex flex-col gap-3">
                 <h1 className="text-[24px] font-semibold">{place?.name}</h1>
                 <p className="underline flex">
@@ -61,8 +65,37 @@ const PlacePage = () => {
                 {place && <PhotoPreview photos={place.photos} />}
             </div>
             <div className="grid grid-cols-3 gap-8 justify-between mt-8">
-                <div className="mb-96 col-span-2">
-                    <div>More Details</div>
+                <div className="col-span-2">
+                    <div className="flex flex-col gap-3 px-4 py-2">
+                        <p className="text-slate-400">Rented By</p>
+                        <div className="flex gap-3 px-4 py-2 rounded-lg bg-slate-300 max-w-[80%]">
+                            <ProfileIconBig />
+                            <div>
+                                <p className="font-bold text-lg">
+                                    {place?.owner?.name}
+                                </p>
+                                <p>{place?.owner?.email}</p>
+                            </div>
+                        </div>
+                        <p className="max-w-[80%] text-justify">{place?.description}</p>
+                    </div>
+                    <div className="flex ml-2 mb-2 py-4 px-6 gap-6 bg-slate-300 rounded-full mt-6 font-semibold text-[#525252] w-fit">
+                        <p className="text-[16px]">₹ {place?.price}</p>
+                        <div className="w-[1px] -my-4 bg-slate-500 rounded-md">
+                            &nbsp;
+                        </div>
+                        <p className="text-[16px] flex gap-2">
+                            <RoomIcon />
+                            Rooms: {place?.rooms}
+                        </p>
+                        <div className="w-[1px] -my-4 bg-slate-500 rounded-md">
+                            &nbsp;
+                        </div>
+                        <p className="text-[16px] flex gap-2">
+                            <BedIcon />
+                            Beds: {place?.beds}
+                        </p>
+                    </div>
                 </div>
                 <div>
                     <BookingForm
