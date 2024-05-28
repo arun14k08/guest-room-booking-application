24c64d4be14d8f69614bc1520622fe91404e1752
ARUN KUMAR D <arun14k08@gmail.com>
2024-05-28 13:41:46 +0530

    fix:typo fixed

--- client/src/pages/AccountPage/components/BookingTile.jsx
+++ client/src/pages/AccountPage/components/BookingTile.jsx
@@ -9,11 +9,9 @@ const BookingTile = ({ booking, place, owner }) => {
         <div className="bg-slate-200 px-8 py-6 rounded-lg flex gap-6">
             <div className="w-[300px] h-[200px] flex justify-center items-center rounded-lg overflow-hidden m-1">
                 <img
-                    src={
-                        import.meta.env.BACKEND_URL +
-                        "/uploads/" +
+                    src={`${import.meta.env.VITE_BACKEND_URL}/uploads/${
                         booking?.place?.photos[0]
-                    }
+                    }`}
                     alt="thumbnail object-contain rounded-lg"
                 />
             </div>
