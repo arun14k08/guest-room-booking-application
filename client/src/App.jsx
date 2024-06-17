07efe2de190a7652f6298d3cb20f88a1aa48357b
ARUN KUMAR D <arun14k08@gmail.com>
2024-06-17 23:06:45 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,6 @@ axios.defaults.withCredentials = true;
 axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.common["Access-Control-Allow-Origin"] =
     "*";
-axios.defaults.headers.common["Access-Control-Allow-Methods"] =
-    "GET, POST, PUT, DELETE, OPTIONS";
 axios.defaults.headers.common["Access-Control-Allow-Headers"] =
     "Content-Type, Authorization";
 
